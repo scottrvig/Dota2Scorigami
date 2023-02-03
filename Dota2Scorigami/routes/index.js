aab5de98c4eadf0a1d761fc5b04b2d9f8eed78d4
scottrvig <scottrvig@gmail.com>
2023-02-02 22:04:59 -0700

    Get matrix from controller. TODO: fill table

--- Dota2Scorigami/routes/index.js
+++ Dota2Scorigami/routes/index.js
@@ -14,10 +14,18 @@ router.get('/getScorigamiMatrix', async (req, res) => {
     // TODO: "Query parameters" for "include The International", Majors, etc
     var query = "SELECT match_id, start_time, dire_score, radiant_score, dire_team_name, radiant_team_name, league_name FROM dbo.Matches";
 
+    var matrix = Array(maxScore).fill().map(() => Array(maxScore).fill(0));
+
     try {
         const pool = await poolPromise;
         const result = await pool.request().query(query);
-        res.json(result.recordset);
+
+        // Build scorigami matrix
+        for (var i = 0; i < result.recordset.length; i++) {
+            matrix[result.recordset[i].radiant_score][result.recordset[i].dire_score]++;
+        }
+
+        res.json(matrix);
     } catch (err) {
         res.status(500);
         res.send(err.message);
