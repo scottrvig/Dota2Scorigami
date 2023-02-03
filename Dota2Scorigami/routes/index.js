0bf792d51a08f86ec9e8830a08c007ba559cf0a3
scottrvig <scottrvig@gmail.com>
2023-02-02 20:35:09 -0700

    Fix async controller calls

--- Dota2Scorigami/routes/index.js
+++ Dota2Scorigami/routes/index.js
@@ -1,24 +1,43 @@
 'use strict';
 var express = require('express');
-var db = require('../services/db');
 var router = express.Router();
+const { poolPromise } = require('../services/db');
+
+var maxScore = 85;
 
 /* GET home page. */
 router.get('/', (req, res) => {
     res.render('index', { title: 'Express' });
 });
 
-router.get('/getScorigamiMatrix', (req, res) => {
+router.get('/getScorigamiMatrix', async (req, res) => {
     // TODO: "Query parameters" for "include The International", Majors, etc
+    var query = "SELECT match_id, start_time, dire_score, radiant_score, dire_team_name, radiant_team_name, league_name FROM dbo.Matches";
 
-    // TODO: Fix async
-    var matrix = db.getScorigamiMatrix();
-    res.send(matrix);
+    try {
+        const pool = await poolPromise;
+        const result = await pool.request().query(query);
+        res.json(result.recordset);
+    } catch (err) {
+        res.status(500);
+        res.send(err.message);
+    }
 });
 
-router.get('/getMatchesWithScore/:radiantScore/:direScore', (req, res) => {
-    var result = db.getMatchesWithScore(radiantScore, direScore);
-    res.send(result);
+router.get('/getMatchesWithScore/:radiantScore/:direScore', async (req, res) => {
+    var query = "SELECT match_id, start_time, dire_score, radiant_score, dire_team_name, radiant_team_name, league_name";
+    query += " FROM dbo.Matches";
+    query += " WHERE radiant_score = " + req.params.radiantScore;
+    query += " AND dire_score = " + req.params.direScore;
+
+    try {
+        const pool = await poolPromise;
+        const result = await pool.request().query(query);
+        res.json(result.recordset);
+    } catch (err) {
+        res.status(500);
+        res.send(err.message);
+    }
 });
 
 module.exports = router;
